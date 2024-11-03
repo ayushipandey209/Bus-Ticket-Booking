34ce9eece7a0996e943552fe0006a2811a32a62f
ayushipandey209 <ayushipandey20903@gmail.com>
2024-11-03 12:36:46 +0530

    updates

---
+++ src/busmanagementsystemproject/BusBookingSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package busmanagementsystemproject;
+/**
+ *
+ * @author slim5
+ */
+public class BusBookingSystem {
+
+    public static void main(String[] args) {
+       MainScreen mr = new MainScreen();
+       mr.setLocationRelativeTo(null);
+       mr.setVisible(true);
+       
+    }
+}
---
+++ src/busmanagementsystemproject/BusManagementSystemProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package busmanagementsystemproject;
+
+/**
+ *
+ * @author slim5
+ */
+public class BusManagementSystemProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainScreen mr = new MainScreen();
+       mr.setLocationRelativeTo(null);
+       mr.setVisible(true);
+       
+    }
+    
+}
